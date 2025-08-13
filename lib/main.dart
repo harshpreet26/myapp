69273a55c17705483a229bd09eedccffaf734a55
harshmaur2004 <harshmaur2004@gmail.com>
2025-08-13 21:07:45 +0000

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,8 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Home_Page(),
-      );
-}
+    );
+  }
 }
 
 class Home_Page extends StatefulWidget {
@@ -23,6 +24,17 @@ class Home_Page extends StatefulWidget {
   State<Home_Page> createState() => _Home_PageState();
 }
 
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+List<Widget> containers =
+    items.map((item) {
+      return Container(
+        color: Colors.blue,
+        height: 200,
+        child: Center(child: Text('$item')),
+      );
+    }).toList();
+
 class _Home_PageState extends State<Home_Page> {
   @override
   Widget build(BuildContext context) {
@@ -31,27 +43,22 @@ class _Home_PageState extends State<Home_Page> {
         backgroundColor: Colors.blue,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children:[
+          children: [
             FlutterLogo(size: 83),
-             Expanded(child: Text('Flutter App')),
-             ],
+            Expanded(child: Text('Flutter App')),
+          ],
         ),
       ),
       drawer: Drawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(color: Colors.blue, height: 200),
-            Divider(),
-             Container(color: Colors.blue, height: 200),
-            Divider(),
-             Container(color: Colors.blue, height: 200),
-            Divider(),
-             Container(color: Colors.blue, height: 200)
-        ],
-        ),
-      ),
-      bottomNavigationBar: const BottomAppBar(color: Colors.red)
+      body: ListView.builder(
+        itemCount: containers.length,
+        itemBuilder: (context, index){
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: containers[index],
+        );
+      }),
+      bottomNavigationBar: const BottomAppBar(color: Colors.red),
     );
   }
 }
